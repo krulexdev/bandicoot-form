fa208e92df1bc3645f1940fe92fefb67bf3c0393
Aditya Akbar <adityakbarp@gmail.com>
2024-12-22 14:04:28 +0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(git diff HEAD^ HEAD)" != "" ]; then
-    echo "Changes detected, building site..."
-    exit 1 
-else
-    echo "No changes detected, skipping build."
-    exit 0 
-fi
