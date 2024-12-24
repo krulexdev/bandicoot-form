7453a2be59a9d0dcaad1acea9bb419d75e03924f
Aditya Akbar <adityakbarp@gmail.com>
2024-12-24 07:23:10 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,4 +60,4 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
 sr.reveal('.home__social-icon', { interval: 200 });
-sr.reveal('.skills__data, .workzilla__img, .support__input', { interval: 200 });
+sr.reveal('.quotes__data, .blurb__img, .support__input', { interval: 200 });
